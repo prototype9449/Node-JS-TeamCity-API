c5b1588086fc947e942287319f84c147d2353693
prototype9449 <prototype9449@yandex.ru>
2015-08-07 16:08:41 +0300

    [MovingToBuildTypes] method from file "socketmanager" was separated

--- api/libs/socketManager.js
+++ api/libs/socketManager.js
@@ -4,43 +4,47 @@ function SocketManager(server, time) {
     this.time = time || 4000;
     //this.io = require('socket.io')(server, { path:  '/api/socket.io' });//IIS
     this.io = require('socket.io')(server, { path:  '/socket.io' });//WebStorm
-    this.buildsHelper = new teamCityHelper("teamCityBuildTypes");
-    this.agentsHelper = new teamCityHelper("teamCityAgents");
+    this.buildHelper = new teamCityHelper("teamCityBuildTypes");
+    this.agentHelper = new teamCityHelper("teamCityAgents");
     this.interval = {};
 };
 
 SocketManager.prototype.sendInfo = function (socket){
 
-    this.buildsHelper.getUpdate(function (newData) {
+    this.buildHelper.getUpdate(function (newData) {
         socket.emit('buildsUpdate', newData);
     });
 
-    this.agentsHelper.getUpdate(function (newData) {
+    this.agentHelper.getUpdate(function (newData) {
         socket.emit('agentsUpdate', newData);
     });
 };
 
 SocketManager.prototype.start = function () {
+    function SetTimer(self, socket) {
+        self.interval = setTimeout(function send() {
+
+            console.log("timer started");
+            self.sendInfo(socket);
+            self.interval = setTimeout(send, self.time);
+
+        }, 0);
+    }
+
     function begin(self) {
          self.io.on('connection', function (socket) {
 
              console.log('New connection');
 
-             self.buildsHelper.getNew(function (newData){
+             self.buildHelper.getNew(function (newData){
                  socket.emit('newBuilds', newData);
              });
 
-             self.agentsHelper.getNew(function (newData) {
+             self.agentHelper.getNew(function (newData) {
                  socket.emit('newAgents', newData);
              });
 
-            self.interval = setTimeout(function send() {
-
-                console.log("timer started");
-                self.sendInfo(socket);
-                self.interval = setTimeout(send, self.time);
-
-            }, 0);
+            SetTimer(self, socket);
         });
     }
 
