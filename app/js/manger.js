1ebb7ffd27f2e4c833f9239ed3b4ff4835a51a7a
GrimRanger <alovchenkov@gmail.com>
2015-08-11 10:00:37 +0300

    [master] Refactoring

--- app/js/manger.js
+++ app/js/manger.js
@@ -24,14 +24,14 @@ $(function () {
     socket.on('newBuilds', function (data) {
         var newBuilds = JSON.parse(data);
         for(var i = 0; i < newBuilds.length; i++){
-            addNewElements(newBuilds[i], 'builds-panel');
+            addNewElements(newBuilds[i].htmlContent, 'builds-panel');
         }
     });
 
     socket.on('buildsUpdate', function (data) {
         var updatingBuilds = JSON.parse(data);
         for(var i = 0; i < updatingBuilds.length; i++){
-            updateElements( updatingBuilds[i], 'builds-panel');
+            updateElements( updatingBuilds[i].htmlContent, 'builds-panel');
         }
     });
 
