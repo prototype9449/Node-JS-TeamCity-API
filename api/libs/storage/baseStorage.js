e451b74a6cf7e686b80dce9707de01712be3b63f
GrimRanger <alovchenkov@gmail.com>
2015-08-25 23:08:23 +0300

    [master] objectStorage was divided into two parts. Second commit

---
+++ api/libs/storage/baseStorage.js
@@ -0,0 +1,54 @@
+var ObjectStorage = function (name) {
+    this.objects = [];
+    this.name = name;
+
+    this.clone = function (object) {
+        var copy = JSON.stringify(object);
+        return JSON.parse(copy);
+    };
+
+    this.getObjectById= function(id, self) {
+        var object = {};
+
+        self.objects.map(function (item) {
+            if (item.id == id) {
+                object = item;
+            }
+        });
+        if (!object) {
+            throw new Error('There is not that id');
+        }
+
+        return object;
+    };
+
+    this.getSpliceArray = function (array, number) {
+        if (!number) return array;
+
+        if (array.length > number) {
+            return array.slice(0, number);
+        } else {
+            return array;
+        }
+    };
+
+    this.push = function (objects) {
+        this.objects = objects;
+    };
+
+    this.get = function (number) {
+        var result = {};
+        result[this.name] = this.getSpliceArray(this.objects, number);
+
+        return result;
+    };
+
+    this.getById = function (id) {
+        var result = {};
+        result[this.name] = [this.clone(this.getObjectById(id, this))];
+
+        return result;
+    };
+};
+
+module.exports.ObjectStorage = ObjectStorage;
